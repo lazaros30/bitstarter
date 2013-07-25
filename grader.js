ddbbc0e09c3326ea3086f6870d4cdaca418e4126
Lazaros Triantafyllidis <lazaros30@gmail.com>
2013-07-25 10:45:56 +0000

    :# Please enter the commit message for your changes. Lines starting

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,14 +62,25 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var file="test.html";
+
+var savefile = function(result,response) {
+	console.log('skata');
+	console.log(result);
+	fs.writeFileSync(file, result);
+}
+
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists),CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url>', 'URL ot check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+		rest.get('http://fierce-reaches-1073.herokuapp.com').on('complete',savefile);
+    //var checkJson = checkHtmlFile(file, program.checks);
+    //var outJson = JSON.stringify(checkJson, null, 4);
+//    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
